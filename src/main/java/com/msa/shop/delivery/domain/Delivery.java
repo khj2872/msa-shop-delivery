17426e250ce75a762aa217b43b2968833559431d
Hyeokjin <gurwls1060@naver.com>
2021-03-30 22:21:34 +0900

    first commit

---
+++ src/main/java/com/msa/shop/delivery/domain/Delivery.java
@@ -0,0 +1,19 @@
+package com.msa.shop.delivery.domain;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Delivery {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+}
