3432abae19995f74d7178d4a908e71ea0d07ce0d
Hyeokjin <gurwls1060@naver.com>
2021-04-07 23:49:54 +0900

    BaseTimeEntity 설정 및 DeliveryId 클래스 분리

--- src/main/java/com/msa/shop/delivery/domain/Delivery.java
+++ src/main/java/com/msa/shop/delivery/domain/Delivery.java
@@ -4,16 +4,14 @@ import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class Delivery {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+public class Delivery extends BaseTimeEntity {
+    @EmbeddedId
+    private DeliveryId id;
+
+    private Long orderId;
 }
---
+++ src/main/java/com/msa/shop/delivery/domain/DeliveryId.java
@@ -0,0 +1,21 @@
+package com.msa.shop.delivery.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+import java.io.Serializable;
+
+@Embeddable
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+public class DeliveryId implements Serializable {
+    @Column(name = "delivery_id")
+    private String id;
+
+}
